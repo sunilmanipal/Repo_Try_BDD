c92a46f925a6b9232ee68f63a9e9671f5342c9f4
sunilna <sunilna@M4L-FAC-SUNIL.mul.manipal.net>
2018-07-25 22:57:15 +0530

    ch

--- New_Test/src/test/java/StepDefinition/StepDefinition.java
+++ New_Test/src/test/java/StepDefinition/StepDefinition.java
@@ -1,21 +1,26 @@
 package StepDefinition;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class StepDefinition {
-	WebDriver driver;
+	
 	@Given("^login app$")
 	public void login_app()  {
 	    // Write code here that turns the phrase above into concrete actions
-		System.setProperty("webdriver.chrome.driver", "D:\\Software\\selenium server\\chromedriver.exe");
-		driver = new ChromeDriver();
+		Capabilities caps = new DesiredCapabilities();
+		((DesiredCapabilities) caps).setJavascriptEnabled(true);
+		//PhantomJSDriver driver;
+		WebDriver driver;
+		((DesiredCapabilities) caps).setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"C:\\PhantomJs\\bin\\Phantomjs\\phantomjs.exe");
+		 driver = new PhantomJSDriver(caps);
 		driver.get("http://opensource-demo.orangehrmlive.com");
 		String title=driver.getTitle();
 		System.out.println("title");
