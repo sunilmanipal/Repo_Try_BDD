0dd8cc80e7de3647d5a4650dc69bab64c6c6530a
sunilna <sunilna@M4L-FAC-SUNIL.mul.manipal.net>
2018-07-25 21:41:45 +0530

    changes

--- New_Test/src/test/java/StepDefinition/StepDefinition.java
+++ New_Test/src/test/java/StepDefinition/StepDefinition.java
@@ -1,13 +1,22 @@
 package StepDefinition;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class StepDefinition {
+	WebDriver driver;
 	@Given("^login app$")
 	public void login_app()  {
 	    // Write code here that turns the phrase above into concrete actions
+		driver = new FirefoxDriver();
+		driver.get("http://opensource-demo.orangehrmlive.com");
+		String title=driver.getTitle();
+		System.out.println("title");
 	System.out.println("Test started");
 	}
 
